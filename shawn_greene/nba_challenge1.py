96dc7aa3e2d534be217bfc69dd15d5da9798ef53
HesKris86 <points2the.watch@gmail.com>
2021-02-03 15:30:47 -0500

    new text shawn_greene

---
+++ shawn_greene/nba_challenge1.py
@@ -0,0 +1,34 @@
+# nba challenge
+# 3pts made
+fred =43
+james = 37
+jamal = 46
+
+a = fred + james
+
+print (a)
+
+# shot attempts
+#player attempt variables
+fv= 110
+jh= 109
+jm= 93
+
+ #player averages:
+vanVleet = (fred/fv)
+harden = (james/jh)
+murray = (jamal/jm)
+
+print("Player averages")
+print(murray)
+print(harden)
+print(vanVleet)
+ # all good up to here 
+
+
+''' op ed text
+The Lakers went all in this offseason and swung a deal for former Pelicans forward Anthony Davis.
+Those three have made good developments with the Pelicans, especially Brandon Ingram.
+Los Angeles has ridden James and Davis, along with a supporting cast built around them, to the second-best record in the NBA.
+They were narrowly behind the Bucks for the best record in the league. 
+'''
