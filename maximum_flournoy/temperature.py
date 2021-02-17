289a502e186d346f2dd5e5368bf1b79e91889db3
Robert Sancez <robsanchez124@gmail.com>
2021-02-16 21:06:31 -0500

    adding my Nest.py

---
+++ maximum_flournoy/temperature.py
@@ -0,0 +1,26 @@
+print('Question 1')
+# convert 100 degrees fahrenheit to celsius
+celsius_100 = (100-32)*5/9 
+print(celsius_100)
+# code is using line 3 to print line 4 in cod
+print('It is a float! There are decimal points')
+
+print('Question 2')
+# convert temperature of 0 degrees fahrenheit to celsius
+celsius_0 = (0-32)*5/9
+print(celsius_0)
+
+print('Question 3')
+# convert a temperature of 34.2 degrees fahrenheit to celsius
+print((34.2-32)*5/9)
+
+print('Question 4')
+# convert 5 degrees celsius to fahrenheit
+print((5*9/5)+32)
+
+print('Question 5')
+# what is hotter, a temp of 30.2 celsius or 85.1 fahrenheit?
+f_30_point_2 =((30.2*9/5)+32)
+print(f_30_point_2)
+# 30.2 celsius is 86.6 fahrenheit
+print('30.2 degrees celsius is hotter')
---
+++ robert_sanchez/temperature.py
@@ -0,0 +1,22 @@
+#Formula for coverting farenheit to celsius
+celsius_100 = ((100-32) * 5//9)
+ 
+#print the results of the conversion
+print(celsius_100)
+
+#updating variable for converting 0 degrees farenheit
+celsius_0 = ((0-32) * 5/9)
+
+#printing result of conversion 2
+print(celsius_0) 
+
+#Converting a temperature of 34.2 degrees farenheit
+print((34.2-32) * 5//9)
+
+#converting a temperature of 5 degrees celsius to fahrenheit
+print((9/5 * 5) + 32)
+
+#tying to find out what's hotter 32.2 or 85.1 Fahrenheit
+print((85.1 - 32) * 5/9)
+
+#30.2 is hotter than 85.1 Fahrenheit.
