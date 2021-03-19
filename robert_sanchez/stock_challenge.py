b719e57606c16be51a549a19c0f92551f6398874
Robert Sancez <robsanchez124@gmail.com>
2021-03-19 18:08:38 -0400

    files/challenges that need to be completed

--- robert_sanchez/stock_challenge.py
+++ robert_sanchez/stock_challenge.py
@@ -1,3 +1,4 @@
+# TODO
 print("Challenge 3.2: Playing with the stock market")
 print()
 # Creating variables to store the current (approximate) market price of these 5 companies - Amazon, Apple, Facebook, Google and Microsoft.
